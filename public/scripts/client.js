8496fd4b5debc1f00ee9be0c10068bb673fa86a4
Maheerah Anwar <maheerahanwar@gmail.com>
2024-12-26 21:31:14 -0500

    Get route and Timeago library

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,28 +1,4 @@
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+
 
 // Function to render multiple tweets
 const renderTweets = function(tweets) {
@@ -50,7 +26,7 @@ const createTweetElement = function(tweet) {
       </header>
       <p>${tweet.content.text}</p>
       <footer>
-        <span>${new Date(tweet.created_at).toLocaleString()}</span>
+        <span>${timeago.format(tweet.created_at)}</span>
         <div>
           <i class="fa-solid fa-flag"></i>
           <i class="fa-solid fa-retweet"></i>
@@ -63,7 +39,7 @@ const createTweetElement = function(tweet) {
   return $tweet; // Return the tweet element
 };
 
-renderTweets(data);
+
 
 //Event listener
 $(function () {
